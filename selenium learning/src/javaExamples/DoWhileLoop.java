0d85bdedbc246002b0d23bacffb94f30bdc8d550
Admin <Admin@DesignExperts07>
2018-07-14 15:31:04 +0530

    included logic for looping

---
+++ selenium learning/src/javaExamples/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package javaExamples;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=10;
+do {
+	System.out.println(i);i++;
+} while (i<10);
+	}
+
+}
---
+++ selenium learning/src/javaExamples/WhileLoopExample.java
@@ -0,0 +1,14 @@
+package javaExamples;
+
+public class WhileLoopExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+while(i>=10){
+	System.out.println(i);i++;
+}
+	
+	}
+
+}
